3a1811e2fe7a8650b2e187215e32ee36e748f42e
Jackson Hales <me@jacksonhales.com.au>
2018-04-12 22:56:45 +1000

    Connected to Azure and retrieved data

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,34 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            SqlConnection sqlCon = new SqlConnection("Server=tcp:thugsquad.database.windows.net,1433;Initial Catalog=ThugCoin;Persist Security Info=False;User ID=tsadmin;Password=Thuglife99;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            sqlCon.Open();
+
+            SqlCommand sqlCom = new SqlCommand("SELECT firstName FROM [Table]", sqlCon);
+
+            SqlDataReader reader = sqlCom.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}", reader.GetString(0));
+                }
+            }
+            else
+            {
+                reader.Close();
+            }
+
+
+
+            /*sqlCom.CommandType = CommandType.Text;
+            sqlCom.Parameters.Add("firstName", SqlDbType.VarChar);
+            sqlCom.Parameters["firstName"].Value = newName;
+            sqlCom = new SqlCommand("SELECT firstName FROM dbo.Table WHERE Id=1", sqlCon);
+            */
+
+            Console.ReadLine();
         }
     }
 }
--- ConsoleApp1/Database1/Table1.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Table1]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [firstName] VARCHAR(50) NULL, 
-    [lastName] VARCHAR(50) NULL
-)
